4b304dc4fe93e285916b618a6eb15bd202f35161
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-12-16 12:58:38 -0500

    Remove unused code

--- video_cube.py
+++ video_cube.py
@@ -16,19 +16,6 @@ for i in range(1, 420):
     if mat is None:
         # create material
         mat = bpy.data.materials.new(name=str(i))
-
-    #mat = bpy.data.materials['Material']
-    #tex = bpy.data.textures.get(str(i))
-    #if tex is None:
-        #tex = bpy.data.textures.new(str(i), "IMAGE")
-    #tex.image = bpy.data.images[str(i).zfill(4) + ".jpg"]
-    
-    #print(tex)
-    
-    #mat.texture_slots.add()
-    #slot = mat.texture_slots[0]
-    #slot.texture = tex
-    #slot.texture_coords = 'Generated'
     
     mat.use_nodes = True;
     texture = mat.node_tree.nodes.new("ShaderNodeTexImage")
@@ -38,8 +25,6 @@ for i in range(1, 420):
     coord = mat.node_tree.nodes.new("ShaderNodeTexCoord")
     vector = texture.inputs["Vector"]
     mat.node_tree.links.new(vector, coord.outputs['Generated'])
-    
-    #mat.node_tree.nodes[“main image”].image = bpy.data.images[str(i).zfill(4) + ".jpg"]
 
     # Assign it to object
     if ob.data.materials:
