566895b025708092c89808b5dbffb867007947a2
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-12-16 13:29:55 -0500

    Automatically combine layers after they are all generated

--- video_cube.py
+++ video_cube.py
@@ -8,10 +8,12 @@ for i in range(1, 420):
     bpy.data.images.load(filepath, check_existing=True)
     #bpy.data.images[name].name = str(i)
 
+layers = []
 for i in range(1, 420):
     bpy.ops.mesh.primitive_cube_add(location=(0, 0, i * 0.02))
     bpy.ops.transform.resize(value=(1, 1, 0.01))
     ob = bpy.context.active_object
+    layers.append(ob)
 
     # Get material
     mat = bpy.data.materials.get(str(i))
@@ -35,3 +37,9 @@ for i in range(1, 420):
     else:
         # no slots
         ob.data.materials.append(mat)
+        
+for layer in layers:
+    layer.select = True
+bpy.ops.object.join()
+bpy.ops.object.origin_set(type="ORIGIN_GEOMETRY")
+bpy.ops.object.select_all(action="TOGGLE")
